b2abb364a3fa2cc48c2694f3119e8158670b616a
opyjo <opeyemiojoj@gmail.com>
2022-05-26 05:17:22 -0400

    First commit

---
+++ src/components/Button.js
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+
+const Button = ({ color, text, onAdd }) => {
+  return (
+    <button onClick={onAdd} style={{ backgroundColor: color }} className="btn">
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: "steelblue",
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+};
+
+export default Button;
